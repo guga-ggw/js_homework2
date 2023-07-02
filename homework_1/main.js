a191640b61745cc133a0d4b6aeccfbc99165269e
guga-ggw <72497475+guga-ggw@users.noreply.github.com>
2023-07-02 13:38:48 +0400

    Add files via upload

---
+++ homework_1/main.js
@@ -0,0 +1,17 @@
+const people = [
+    { name: 'Temo', age: 25 },
+    { name: 'Lasha', age: 21 },
+    { name: 'Ana', age: 28 }
+  ];
+
+  let lowestage = 200
+  let human 
+  people.forEach((user) =>{
+    if(lowestage > user.age ){
+        lowestage = user.age
+        return human = user
+    }
+  } )
+
+  console.log(`${human.name} არის ${lowestage} წლის და ყველაზე უმცროსი სხვებთან შედარებით
+   `)
